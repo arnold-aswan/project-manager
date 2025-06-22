1756242a8a99b79ab18640db0ab63fdf53c2132e
arnold-aswan <fredyaswan@gmail.com>
2025-06-22 14:48:01 +0300

    feat: Refactor useAuth store to include persistence and improve state management

--- frontend/app/hooks/useAuth.ts
+++ frontend/app/hooks/useAuth.ts
@@ -1,6 +1,9 @@
 import { useMutation } from "@tanstack/react-query";
 import type { SignUpFormData } from "@/routes/auth/sign-up";
 import { postData } from "@/lib/fetch-utils";
+import type { SignInFormData } from "@/routes/auth/sign-in";
+import type { ForgotPasswordRequestFormData } from "@/routes/auth/forgot-password";
+import type { ResetPasswordFormData } from "@/routes/auth/reset-password";
 
 export const useSignUpMutation = () => {
 	return useMutation({
@@ -12,5 +15,25 @@ export const useVerifyEmailMutation = () => {
 	return useMutation({
 		mutationFn: (data: { token: string }) =>
 			postData("/auth/verify-email", data),
+	});
+};
+
+export const useLoginMutation = () => {
+	return useMutation({
+		mutationFn: (data: SignInFormData) => postData("/auth/login", data),
+	});
+};
+
+export const useForgotPasswordMutation = () => {
+	return useMutation({
+		mutationFn: (data: ForgotPasswordRequestFormData) =>
+			postData("/auth/reset-password-request", data),
+	});
+};
+
+export const useResetPasswordMutation = () => {
+	return useMutation({
+		mutationFn: (data: ResetPasswordFormData) =>
+			postData("/auth/reset-password", data),
 	});
 };
--- frontend/app/stores/authstore.ts
+++ frontend/app/stores/authstore.ts
@@ -1,11 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import type { AuthState, User } from "@/types";
+import { queryClient } from "@/providers/react-query-provider";
 
-const useAuthStore = create<AuthState>((set) => ({
-	isAuthenticated: false,
-	user: null,
-	loginSuccess: (user: User) => set({ isAuthenticated: true, user }),
-	logout: () => set({ isAuthenticated: false, user: null }),
-}));
+const useAuthStore = create<AuthState>()(
+	persist(
+		(set) => ({
+			isAuthenticated: false,
+			user: null,
+			loginSuccess: (user: User) => set({ isAuthenticated: true, user }),
+			logout: () => {
+				set({ isAuthenticated: false, user: null });
+				queryClient.clear();
+			},
+		}),
+		{
+			name: "taskhub-auth",
+		}
+	)
+);
 
 export default useAuthStore;
