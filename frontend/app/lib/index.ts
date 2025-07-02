e6174da2496b0bf3ed134d831bcd8622102ef2e0
arnold-aswan <fredyaswan@gmail.com>
2025-07-02 11:12:30 +0300

    feat: Add dateFormatter for consistent date formatting in Kenyan locale

--- frontend/app/lib/index.ts
+++ frontend/app/lib/index.ts
@@ -18,3 +18,11 @@ export const colorOptions = [
 	"#2ECC71",
 	"#34495E",
 ];
+
+// Date converter
+export const dateFormatter = new Intl.DateTimeFormat("en-KE", {
+	dateStyle: "medium",
+	timeStyle: "short",
+	hour12: true,
+});
+// returns "Jun 25, 2025, 4:06 PM" (Kenyan time)
