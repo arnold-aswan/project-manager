26a84f20f5e6d623e4f334526ab48f365cbd1949
arnold-aswan <fredyaswan@gmail.com>
2025-07-02 11:13:03 +0300

    feat: Define Project, Task, SubTask, and Attachment interfaces with corresponding enums for project management

--- frontend/app/types/index.ts
+++ frontend/app/types/index.ts
@@ -10,6 +10,7 @@ export interface User {
 	updatedAt: Date;
 	lastLogin: string;
 	profilePicture?: string;
+	avatar?: string;
 	__v: number;
 }
 export interface AuthState {
@@ -34,12 +35,90 @@ export interface Workspace {
 	updatedAt: Date;
 }
 
+export interface Project {
+	_id: string;
+	title: string;
+	description?: string;
+	workspace: Workspace;
+	status: ProjectStatus;
+	startDate: Date;
+	dueDate: Date;
+	progress: Number;
+	task: Task[];
+	members: {
+		user: User;
+		role: "admin" | "member" | "owner" | "viewer";
+	};
+	tags?: string[];
+	createdBy: User;
+	isArchived: boolean;
+}
+
+export enum ProjectStatus {
+	PLANNING = "Planning",
+	IN_PROGRESS = "In Progress",
+	ON_HOLD = "On Hold",
+	COMPLETED = "Completed",
+	CANCELLED = "Cancelled",
+}
+
+export interface Task {
+	_id: string;
+	title: string;
+	description?: string;
+	project: Project;
+	status: TaskStatus;
+	priority: "Low" | "Medium" | "High";
+	assignees: User[];
+	watchers?: User[];
+	dueDate: Date;
+	completedAt: Date;
+	estimatedHours: number;
+	actualHours: number;
+	tags?: string[];
+	subTasks?: SubTask[];
+	// comments: Types.ObjectId[];
+	attachments?: Attachment[];
+	createdBy: User | string;
+	isArchived: boolean;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export enum TaskStatus {
+	TO_DO = "TO DO",
+	IN_PROGRESS = "In Progress",
+	REVIEW = "Review",
+	DONE = "Done",
+}
+
+export interface SubTask {
+	title: string;
+	completed?: boolean;
+	createdAt?: Date;
+}
+
+export interface Attachment {
+	fileName: string;
+	fileUrl: string;
+	fileType?: string;
+	fileSize?: number;
+	uploadedBy?: User | string;
+	uploadedAt?: Date;
+}
+
 interface SidebarNavItems {
 	title: string;
 	href: string;
 	icon: LucideIcon;
 }
 [];
+export interface NoDataFoundProps {
+	title: string;
+	description: string;
+	buttonText: string;
+	buttonAction: () => void;
+}
 
 // COMPONENT PROPS
 export interface HeaderProps {
@@ -58,4 +137,24 @@ export interface SidebarNavProps {
 export interface CreateWorkspaceProps {
 	isCreatingWorkspace: boolean;
 	setIsCreatingWorkspace: (isCreatingWorkSpace: boolean) => void;
+}
+
+export interface ProjectCardProps {
+	project: Project;
+	progress: Number;
+	workspaceId: string;
+}
+
+export interface ProjectDialogProps {
+	isOpen: boolean;
+	onOpenChange: (open: boolean) => void;
+	workspaceId: string;
+	workspaceMembers: MemberProps;
+}
+
+export interface MemberProps {
+	_id: string;
+	user: User;
+	role: "admin" | "member" | "owner" | "viewer";
+	joinedAt: Date;
 }
